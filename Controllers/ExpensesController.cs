af595ab4ba1b43372ae5bf5630266da9639125bf
Justin Simon <justinsimoncs@gmail.com>
2025-09-05 20:13:44 -0700

    2ndd commit

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TutorialWindowsMVC.Data;
+
+namespace TutorialWindowsMVC.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly FinanceAppContext _context;
+        public ExpensesController(FinanceAppContext context) 
+        {
+
+            _context = context;
+            
+        }
+        public IActionResult Index()
+        {
+            var expenses = _context.Expenses.ToList();
+            return View(expenses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+
+
+        }
+    }
+}
---
+++ Data/FinanceAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorialWindowsMVC.Models;
+
+namespace TutorialWindowsMVC.Data
+{
+    public class FinanceAppContext: DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options)
+        {
+            
+        }
+
+       public DbSet<Expense> Expenses { get; set; }
+    }
+}
